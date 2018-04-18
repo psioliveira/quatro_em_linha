e57c3b3bd01ecc47d7c8bf9a2138d5cdd44a030d
pedrooliveira2007 <pedrooliveira2007@hotmail.com>
2018-04-18 23:11:43 +0100

    constructors and methods added

--- Simplexity/Block.cs
+++ Simplexity/Block.cs
@@ -11,7 +11,27 @@ namespace Simplexity
         //Shape and color properties (auto implemented)
         public bool form  { get; set; }
         public string color { get; set; } = "";
-        
+
+        // Constructors
+        public Block(bool form)
+        {
+            this.form = form;
+            color = "";
+            
+        }
+
+        // Methods
+
+        public void Red()
+        {
+            color = "red";
+        }
+
+
+        public void White()
+        {
+            color = "White";
+        }
 
     }
 
