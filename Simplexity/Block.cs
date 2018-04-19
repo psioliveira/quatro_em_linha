ac33d312e21871dcdfb37ad3de843e60a6b90737
pedrooliveira2007 <pedrooliveira2007@hotmail.com>
2018-04-19 12:19:05 +0100

    BelongsTo added

--- Simplexity/Block.cs
+++ Simplexity/Block.cs
@@ -11,6 +11,7 @@ namespace Simplexity
         //Shape and color properties (auto implemented)
         public int form  { get; set; } = Shape.Undecided;
         public string color { get; set; } = "";
+        public int belongsTo {get; private set;} = Shape.Undecided;
 
         // Constructors
         public Block(int form)
@@ -25,12 +26,14 @@ namespace Simplexity
         public void Red()
         {
             color = "red";
+            belongsTo = 1;
         }
 
 
         public void White()
         {
             color = "White";
+             belongsTo = 2;
         }
 
     }
