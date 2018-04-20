d70e61a87e3d164b8ee61491510b0e4c6abbe964
DaPikachuOnMeth <hugomrelvasmartins@gmail.com>
2018-04-20 15:10:15 +0100

    Added setBlockBoard

--- Simplexity/Board.cs
+++ Simplexity/Board.cs
@@ -6,61 +6,66 @@ using System.Threading.Tasks;
 
 namespace Simplexity
 {
-    public class Board
+    class Board
     {
-        internal Block[,] block;
-        
+        private Block[,] board;
+        public int NextTurn { get; set; } = 1;
 
+ 
         // Constructors 
 
         public Board()
         {
-            block = new Block[7, 7];
-           int NextTurn = 1;    
-            
+            board = new Block[7, 7];
         }
-        
 
+<<<<<<< HEAD
         // Getters
       
         private Block GetBlockPos (Position position)
         {
             return state[position.Row, position.Column];
-
-        }
-
-
-        // Setterrs 
-
-        public bool SetBlockPos(Position position, Block newBlock)
-        {
-            if ( newBlock.belongsTo != NextTurn) return false;
-            if (block[position.Row, position.Column].form != State.Undecided) return false;
-
-            block[position.Row, position.Column] = newBlock;
-            SwitchNextTurn(NewBlock);
-            return true;
-
-        }
+=======
+>>>>>>> feb87ccd1b8cd529e4eeb41be897d8ecdfd0b5f4
 
 
         // Methods
 
-            
-        private void SwitchNextTurn( Block BlockPlaced)
+
+        public Block GetBlockBoard (Position position)
         {
-            if (BlockPlaced.color == "white")
+            return board[position.Row, position.Column];
+        }
+
+
+        public bool SetBlockBoard(Position position, Block NewBlock)
+        {
+            {
+                if (NewBlock.BelongsTo != NextTurn) return false;
+                if (board[position.Row, position.Column].Form != 0) return false;
+
+                board[position.Row, position.Column] = NewBlock;
+                SwitchNextTurn(NewBlock);
+                return true;
+            }
+        }
+
+
+
+        private void SwitchNextTurn(Block BlockPlaced)
+        {
+            if (BlockPlaced.Color == "white")
             {
                 NextTurn = 2;
             }
 
-            if (BlockPlaced.color == "red")
+            if (BlockPlaced.Color == "red")
             {
                 NextTurn = 1;
             }
 
-            else NextTurn = State.Undefined;
-                
+            else NextTurn = 0;
+
         }
     }
 
