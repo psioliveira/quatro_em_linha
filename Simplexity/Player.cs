6bdd947485a8748dcbebae88fccf76b015f7fe8d
DaPikachuOnMeth <hugomrelvasmartins@gmail.com>
2018-04-20 14:04:02 +0100

    Added position scanner

--- Simplexity/Player.cs
+++ Simplexity/Player.cs
@@ -13,6 +13,8 @@ namespace Simplexity
         private Block cube = new Block(Shape.cub); //square
         private Block cylinder = new Block(Shape.cil); //cylinder
         private int number;
+        private Block piece_played = new Block(Shape.Undecided); //Usado para verificar jogada
+            
 
         //Proprieties 
 
@@ -39,12 +41,28 @@ namespace Simplexity
             }
         }
 
-        //Methods   
+        //verifica onde vai adicionar a peça no tabuleiro e se a mesma entrada está vazia    
+        //
+        public Position GetPosition (Board board)
+         {
+            int piece_type = (int)Console.ReadKey();
+            int column_num = (int)Console.ReadKey(); 
+            for (i= 6; i >= 0; i--)
+7           {
+               if( board[i,column_num].Form == 0)
+                    return Position(i, column_num);
+               //add piece 
 
-        public void Piece_payed()
+            }
+        }
+        public void Piece_played(Block blockp)
         {
-
-
+            //Swap turn
+            
+            //Scan current turn's piece
+            private int current_play;
+            current_play ;
+            
         }
     }
 }
